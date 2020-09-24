1e24adf083f2ee1047bc487e084ce7a10755e221
Martin Hinsch <hinsch.martin@gmail.com>
2020-09-24 19:01:04 +0100

    license

--- params.jl
+++ params.jl
@@ -1,3 +1,20 @@
+#   Copyright (C) 2020 Martin Hinsch <hinsch.martin@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 # This package provides some nice convenience syntax for parameters.
 # Specifically we can set default values for struct elements. A constructor is
 # then created that a) has all struct elements as named args and b) sets the
